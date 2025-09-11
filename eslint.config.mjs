2bf6649aca3b5208588d2e8fb5a1f42bf1969e08
Pratik Ghimire <147188683+pratik-gmre@users.noreply.github.com>
2025-09-12 00:02:31 +0545

    Update eslint.config.mjs

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -11,14 +11,7 @@ const compat = new FlatCompat({
 
 const eslintConfig = [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
-  {
-    plugins: {
-      "@typescript-eslint": require("@typescript-eslint/eslint-plugin"),
-    },
-    rules: {
-      "@typescript-eslint/no-unused-vars": ["error"],
-    },
-  },
+ 
 ];
 
 export default eslintConfig;
